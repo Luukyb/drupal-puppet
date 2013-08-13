8bfd7d63b13c8ff1e5bafe97d5c301d8a2d77eaa
Luc Bezier <luc@webizat.com>
2013-08-13 15:43:06 +0800

    Using example42 apache module

--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -5,41 +5,27 @@ class { 'apt':
   purge_sources_list_d => false,
   purge_preferences_d  => false
 }
+include apt
 
-# Installs default Apache modules based on lucid
-class { 'apache':
-  default_mods => true,
-  mpm_module => 'prefork',
-}
+class { 'apache': }
 
-# TODO: https://drupal.org/requirements/php
-include apache::mod::php
-
-apache::mod { 'rewrite': }
-
-# Clean URL requirements 
-# @see https://drupal.org/node/15365
-apache::vhost { 'vagrant.local':
-  docroot => '/var/www/drupal-project',
-  #options => 'Indexes', 'MultiViews', 'FollowSymLinks',
-  directories => [ { path => '/var/www/drupal-project', allow_override => ['All'] }],
-  #directories => [ { path => '/var/www/drupal-project, allow_override => ['AuthConfig', 'Indexes'] } ],
-  port    => '80',
-  rewrite_base => '/',
-  # rewrite_cond => [ '%{REQUEST_FILENAME} !-f', '%{REQUEST_FILENAME} !-d'],
-  # rewrite_rule => '^(.*)$ index.php?q=$1 [L,QSA]',
-}
-
-#class webserver {
-  # Setups the virtual host
-#  file { '/etc/apache2/sites-enabled/site.conf':
-#    source  => '/vagrant/puppet/config/site.conf',
-#    notify  => Service['apache2'],
-#    require => Package['apache2'],
-#  }
+#apache::dotconf { 'custom':
+#  content => 'EnableSendfile Off',
 #}
 
-#include webserver
+apache::module { 'rewrite': }
+
+apache::vhost { 'vagrant.local':
+  server_name   => 'vagrant.local',
+  serveraliases => [
+    'www.vagrant.local'
+  ],
+  docroot       => '/vagrant/drupal-project',
+  port          => '80',
+  env_variables => [],
+  priority      => '1',
+  directory_allow_override   => 'All',
+}
 
 class { 'mysql::server':
   config_hash => { 'root_password' => 'root' }
